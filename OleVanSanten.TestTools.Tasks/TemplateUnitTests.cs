4e86da0635192e07d8ce555dfdfbaf25f8f406ef
Ole van Santen <ovs@netcompany.com>
2021-08-23 21:07:48 +0200

    Something is working

---
+++ OleVanSanten.TestTools.Tasks/TemplateUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace OleVanSanten.TestTools.Tasks
+{
+    public class TemplateUnitTest : Task
+    {
+        [Output]
+        public string ConfigPath { get; set; }
+
+        public override bool Execute()
+        {
+            Log.LogMessage("Hello world");
+            return true;
+        }
+    }
+}
---
+++ OleVanSanten.TestTools/UnitTestTemplator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Emit;
+//using Microsoft.CodeAnalysis.MSBuild;
+
+namespace OleVanSanten.TestTools
+{
+    public class UnitTestTemplator
+    {
+        public void TemplateProject(string solutionPath)
+        {
+            //MSBuildWorkspace workspace = MSBuildWorkspace.Create();
+
+        }
+    }
+}
